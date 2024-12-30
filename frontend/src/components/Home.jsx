3ae7b400238ca00de638c24d724bad2f76a1a924
kushalswami20 <kushalswami56@gmail.com>
2024-12-30 21:06:52 +0530

    deploy

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -487,7 +487,7 @@ Senior Secondary School (10TH) with a score of 92%
             </a>
         </div>
         <p className="mt-8 text-base leading-6 text-center text-gray-400">
-            © 2021 SomeCompany, Inc. All rights reserved.
+            © 2024 KUSHAL AROGYA SWAMI, Inc. All rights reserved.
         </p>
         </div>
     
